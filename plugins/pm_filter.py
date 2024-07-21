70c396cd163fab81c7f12823339ab0c8f615d7b6
Hdhshsjsjsj <ilashadam039@gmail.com>
2024-07-21 17:15:06 +0400

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -592,7 +592,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]         
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.SUR_TXT.format(query.from_user.mention, temp.text=script.START_TXT.format(query.from_user.mentionU_NAME, temp.B_NAME),
+            text=script.SUR_TXT.format(query.from_user.mention),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
         )     
